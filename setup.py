2426f2ad63bb20e6033c2b062f8602c967b246b9
Rohith Ambarish <rohithambarish@gmail.com>
2025-05-07 14:18:14 -0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Rohith Ambarish',
+    author_email= 'rohithambarish@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
